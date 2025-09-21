82de2289d1f2f45bc0dc5f3659e495abb873b97f
respectevery01 <2849288402@qq.com>
2025-09-21 15:35:18 +0800

    rm deadlink

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   head: [
     ['link', { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css' }],
   ],
+  ignoreDeadLinks: true,
   
   vite: {
     css: {
